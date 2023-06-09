ed03d32c597f1e63acdee2cc8991f7c26e22913f
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-06-09 15:12:31 +0300

    Load command delivers no images on seven days old cache

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -32,13 +32,13 @@ public final class LocalFeedLoader {
     }
 
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { result in
+        store.retrieve { [unowned self] result in
             switch result {
             case .failure(let error):
                 completion(.failure(error))
-            case .found(let feed, _):
+            case .found(let feed, let timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
-            case .empty:
+            case .found, .empty:
                 completion(.success([]))
             }
         }
@@ -50,6 +50,15 @@ public final class LocalFeedLoader {
 
             completion(error)
         }
+    }
+
+    private func validate(_ timestamp: Date) -> Bool {
+        let calendar = Calendar(identifier: .gregorian)
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else {
+            return false
+        }
+
+        return currentDate() < maxCacheAge
     }
 }
 
