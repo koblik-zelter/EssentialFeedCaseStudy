0543faee07924a664c8e475076bffa6de2ef1d53
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-06-09 13:08:25 +0300

    Move FeedStore to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Alexandr Coblic-Zelter on 09.06.2023.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
