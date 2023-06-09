18c4710316b7ba939fcc7c427680309d116d860b
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-06-09 15:56:59 +0300

    Load command deletes cache on retrieval error

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -36,6 +36,7 @@ public final class LocalFeedLoader {
         store.retrieve { [unowned self] result in
             switch result {
             case .failure(let error):
+                self.store.deleteCachedFeed { _ in }
                 completion(.failure(error))
             case .found(let feed, let timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
