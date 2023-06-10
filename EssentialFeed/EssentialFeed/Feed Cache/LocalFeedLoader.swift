00ff1f10302dd925d5d4dd74ec4eaa99978c0871
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-06-10 09:33:44 +0300

    Extract cache deletion side-effects on expired cache from the load method to the validateCache method

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -41,7 +41,6 @@ public final class LocalFeedLoader {
             case .found(let feed, let timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
             case .found:
-                self.store.deleteCachedFeed { _ in }
                 fallthrough
             case .empty:
                 completion(.success([]))
@@ -62,7 +61,9 @@ public final class LocalFeedLoader {
             switch result {
             case .failure:
                 self.store.deleteCachedFeed { _ in }
-            default: break
+            case let .found(_, timestamp) where !self.validate(timestamp):
+                self.store.deleteCachedFeed { _ in }
+            case .empty, .found: break
             }
         }
     }
