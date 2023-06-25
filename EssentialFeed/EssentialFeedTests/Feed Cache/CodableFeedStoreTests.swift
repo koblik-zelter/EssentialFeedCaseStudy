c87d6fa8255b6fcd693cfbf9c09d958d8e5041b8
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-06-25 17:06:43 +0300

    Retrieving from empty cache delivers empty result

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -0,0 +1,36 @@
+//
+//  CodableFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Alexandr Coblic-Zelter on 25.06.2023.
+//
+
+import XCTest
+import EssentialFeed
+
+final class CodableFeedStore {
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        completion(.empty)
+    }
+}
+
+final class CodableFeedStoreTests: XCTestCase {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "Wait for cache retrieval")
+
+        sut.retrieve { result in
+            switch result {
+            case .empty:
+                break
+
+            default:
+                XCTFail("Expected empty result, got \(result) instead")
+            }
+
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1.0)
+    }
+}
