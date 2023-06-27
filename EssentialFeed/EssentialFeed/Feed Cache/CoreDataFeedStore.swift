6c31d56046f02785d3e570709d68509d64c24a0d
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-06-27 22:57:10 +0300

    CoreDataFeedStore.deleteCachedFeed() empties previously inserted cache

--- EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -50,7 +50,15 @@ public final class CoreDataFeedStore: FeedStore {
     }
 
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        completion(nil)
+        let context = self.context
+        context.perform {
+            do {
+                try ManagedCache.find(in: context).map(context.delete)
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+        }
     }
 }
 
