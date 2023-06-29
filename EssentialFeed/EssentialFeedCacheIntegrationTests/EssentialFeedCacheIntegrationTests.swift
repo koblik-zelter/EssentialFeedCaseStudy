78fb48b02718c8cee20fcffb0cd41e451ffba055
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-06-29 23:26:00 +0300

    Add EssentialFeedCacheIntegrationTests target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,11 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Alexandr Coblic-Zelter on 29.06.2023.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+}
