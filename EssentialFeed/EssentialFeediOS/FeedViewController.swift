a6df672945c015382dd6a15af453a58e6741e47c
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-06-30 22:03:30 +0300

    Move FeedViewController to production target

---
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FeedViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Alexandr Coblic-Zelter on 30.06.2023.
+//
+
+import UIKit
+import EssentialFeed
+
+public final class FeedViewController: UITableViewController {
+    private var loader: FeedLoader?
+
+    public convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        refreshControl?.beginRefreshing()
+        load()
+    }
+
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+}
