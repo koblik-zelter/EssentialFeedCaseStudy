07b71270d2bdbfdf29109f26790fa80b9a30a565
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-01 21:14:41 +0300

    Move FeedErrorViewModel to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Alexandr Coblic-Zelter on 01.07.2023.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
