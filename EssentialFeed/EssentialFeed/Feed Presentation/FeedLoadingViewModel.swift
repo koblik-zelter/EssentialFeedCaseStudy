56dd7cc84fecc3732b21479f13e9315b15db275d
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-01 22:17:13 +0300

    Move FeedLoadingViewModel to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Alexandr Coblic-Zelter on 01.07.2023.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
