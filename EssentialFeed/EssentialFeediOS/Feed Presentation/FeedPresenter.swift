269c2b4eaa1c40e52505ed2947d0c6a0d4864b42
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-01 17:56:16 +0300

    Add FeedPresenter holding feed loading presentation logic (loading state and loaded feed)

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -0,0 +1,38 @@
+//
+//  FeedPresenter.swift
+//  EssentialFeediOS
+//
+//  Created by Alexandr Coblic-Zelter on 01.07.2023.
+//
+
+import EssentialFeed
+
+protocol FeedLoadingView: AnyObject {
+    func display(isLoading: Bool)
+}
+
+protocol FeedView {
+    func display(feed: [FeedImage])
+}
+
+final class FeedPresenter {
+    typealias Observer<T> = (T) -> Void
+
+    var feedView: FeedView?
+    weak var loadingView: FeedLoadingView?
+    private let feedLoader: FeedLoader
+
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+
+    func loadFeed() {
+        loadingView?.display(isLoading: true)
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.feedView?.display(feed: feed)
+            }
+            self?.loadingView?.display(isLoading: false)
+        }
+    }
+}
