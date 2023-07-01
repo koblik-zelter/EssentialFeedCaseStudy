96fe0d91b01bf9333ee4e14a493ead9969c50c9f
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-01 22:35:28 +0300

    Add fail image transformation helper to clarify intent in test setup

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -81,7 +81,7 @@ final class FeedImagePresenterTests: XCTestCase {
     }
 
     func test_didFinishLoadingImageData_displaysRetryOnFailedImageTransformation() {
-        let (sut, view) = makeSUT(imageTransformer: { _ in nil })
+        let (sut, view) = makeSUT(imageTransformer: fail)
         let image = uniqueImage()
         let data = Data()
 
@@ -109,6 +109,10 @@ final class FeedImagePresenterTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
 
         return (sut, view)
+    }
+
+    private var fail: (Data) -> Any? {
+        { _ in nil }
     }
 
     private class ViewSpy: FeedImageView {
