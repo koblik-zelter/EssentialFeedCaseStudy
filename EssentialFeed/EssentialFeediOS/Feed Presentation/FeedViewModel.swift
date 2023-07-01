e8332bb986243fcd8f231d1564a9d0ccf0e4c172
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-01 18:31:56 +0300

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Alexandr Coblic-Zelter on 01.07.2023.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
