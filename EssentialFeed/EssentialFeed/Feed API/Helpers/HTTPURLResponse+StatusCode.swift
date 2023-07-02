bd72e3863e58b22f161344cfc8ac55f6454d96a4
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-02 13:02:08 +0300

    Extract duplicate HTTPURLResponse.statusCode == 200 validation into a isOK helper method

---
+++ EssentialFeed/EssentialFeed/Feed API/Helpers/HTTPURLResponse+StatusCode.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPURLResponse+StatusCode.swift
+//  EssentialFeed
+//
+//  Created by Alexandr Coblic-Zelter on 02.07.2023.
+//
+
+import Foundation
+
+extension HTTPURLResponse {
+    private static var OK_200: Int { return 200 }
+
+    var isOK: Bool {
+        statusCode == HTTPURLResponse.OK_200
+    }
+}
