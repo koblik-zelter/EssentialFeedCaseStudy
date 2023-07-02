46daab45a43119d00c0d653844ff2279fa1feaa8
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-02 10:47:31 +0300

    RemoteFeedImageDataLoader.loadImageDataFromURL delivers non-empty received data on 200 HTTP response

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -22,7 +22,12 @@ final class RemoteFeedImageDataLoader {
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case .success: completion(.failure(Error.invalidData))
+            case .success((let data, let response)):
+                if response.statusCode == 200, !data.isEmpty {
+                    completion(.success(data))
+                } else {
+                    completion(.failure(Error.invalidData))
+                }
             case .failure(let error): completion(.failure(error))
             }
         }
@@ -83,7 +88,17 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
             let emptyData = Data()
             client.complete(withStatusCode: 200, data: emptyData)
         })
-     }
+    }
+
+    func test_loadImageDataFromURL_deliversReceivedNonEmptyDataOn200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        let nonEmptyData = Data("non-empty data".utf8)
+        
+        expect(sut, toCompleteWith: .success(nonEmptyData), when: {
+            client.complete(withStatusCode: 200, data: nonEmptyData)
+        })
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(
