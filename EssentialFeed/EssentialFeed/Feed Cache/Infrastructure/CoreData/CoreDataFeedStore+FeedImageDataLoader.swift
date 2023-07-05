7c03bbb05d609fc1e87c058a8cc784804f01cc09
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-05 23:10:26 +0300

    Move CoreDataFeedStore implementation of the FeedImageDataStore protocol to production

---
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataFeedStore+FeedImageDataLoader.swift
+//  EssentialFeed
+//
+//  Created by Alexandr Coblic-Zelter on 05.07.2023.
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        
+    }
+
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+}
