2a2e733300ae3abf908045a213c7107f5e8cb32e
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-05 22:51:13 +0300

    LocalFeedImageDataLoader.saveImageDataForURL requests image data insertion into store

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -15,6 +15,7 @@ final class FeedImageDataStoreSpy: FeedImageDataStore {
     }
 
     private(set) var retrievalCompletions: [(FeedImageDataStore.RetrievalResult) -> Void] = []
+    private(set) var insertionCompletions: [(FeedImageDataStore.InsertionResult) -> Void] = []
     private(set) var receivedMessages = [Message]()
 
     func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
@@ -28,6 +29,7 @@ final class FeedImageDataStoreSpy: FeedImageDataStore {
         completion: @escaping (FeedImageDataStore.InsertionResult) -> Void
     ) {
         receivedMessages.append(.insert(data: data, for: url))
+        insertionCompletions.append(completion)
     }
 
     func completeRetrieval(with error: Error, at index: Int = 0) {
@@ -36,5 +38,9 @@ final class FeedImageDataStoreSpy: FeedImageDataStore {
 
     func completeRetrieval(with data: Data?, at index: Int = 0) {
         retrievalCompletions[index](.success(data))
+    }
+
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](.failure(error))
     }
 }
