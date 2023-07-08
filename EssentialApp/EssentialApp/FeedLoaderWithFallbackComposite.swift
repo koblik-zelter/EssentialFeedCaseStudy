fc977024c0e27ad8f19b6a16b6209683712307c3
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-08 17:52:24 +0300

    Move FeedLoaderWithFallbackComposite to production

---
+++ EssentialApp/EssentialApp/FeedLoaderWithFallbackComposite.swift
@@ -0,0 +1,29 @@
+//
+//  FeedLoaderWithFallbackComposite.swift
+//  EssentialApp
+//
+//  Created by Alexandr Coblic-Zelter on 08.07.2023.
+//
+
+import EssentialFeed
+
+public final class FeedLoaderWithFallbackComposite: FeedLoader {
+    private let primary: FeedLoader
+    private let fallback: FeedLoader
+
+    public init(primary: FeedLoader, fallback: FeedLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        primary.load { [weak self] result in
+            switch result {
+            case .success:
+                completion(result)
+            case .failure:
+                self?.fallback.load(completion: completion)
+            }
+        }
+    }
+}
