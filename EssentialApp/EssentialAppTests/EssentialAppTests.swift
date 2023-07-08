b3b5c3ae1d23ff5952f10f9f3520b45c49acacb5
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-08 17:34:32 +0300

    Create App test target

---
+++ EssentialApp/EssentialAppTests/EssentialAppTests.swift
@@ -0,0 +1,11 @@
+//
+//  EssentialAppTests.swift
+//  EssentialAppTests
+//
+//  Created by Alexandr Coblic-Zelter on 08.07.2023.
+//
+
+import XCTest
+
+final class EssentialAppTests: XCTestCase {
+}
