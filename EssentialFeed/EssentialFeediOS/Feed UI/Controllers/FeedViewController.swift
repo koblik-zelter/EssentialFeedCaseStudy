d7058a95dba25b113b646a983eb116059ceff8d0
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-09 13:30:20 +0300

    Fix table header resizing for multi-line label

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -27,6 +27,12 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
         refresh()
     }
 
+    override public func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        tableView.sizeTableHeaderToFit()
+    }
+
     @IBAction private func refresh() {
         delegate?.didRequestFeedRefresh()
     }
