a82999e3001520dd2dc8c1a40a9354fdf3d55be0
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-09 14:11:17 +0300

    Improve a test name

--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -10,7 +10,7 @@ import EssentialFeediOS
 @testable import EssentialApp
 
 final class SceneDelegateTests: XCTestCase {
-    func test_sceneWillConnectToSession_configuresRootViewController() {
+    func test_configureWindow_configuresRootViewController() {
         let sut = SceneDelegate()
         sut.window = UIWindow()
 
