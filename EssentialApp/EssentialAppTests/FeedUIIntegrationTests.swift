2fb5b911e35d0db60dc1f52fe1570a03b1d48cb4
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-10 23:24:02 +0300

    Replace FEED_VIEW_CONNECTION_ERROR key with GENERIC_CONNECTION_ERROR

--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -101,7 +101,7 @@ final class FeedUIIntegrationTests: XCTestCase {
         sut.loadViewIfNeeded()
 
         loader.completeFeedLoadingWithError(at: 0)
-        XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
+        XCTAssertEqual(sut.errorMessage, localized("GENERIC_CONNECTION_ERROR"))
 
         sut.simulateUserInitiatedFeedReload()
         XCTAssertEqual(sut.errorMessage, nil)
