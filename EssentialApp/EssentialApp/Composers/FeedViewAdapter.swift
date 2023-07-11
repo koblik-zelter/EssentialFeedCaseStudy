561cc7e40554053706469e24f32a59eda195c9a7
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-11 22:56:51 +0300

    Move UIImage creation to a tryMake extension

--- EssentialApp/EssentialApp/Composers/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/Composers/FeedViewAdapter.swift
@@ -33,12 +33,7 @@ final class FeedViewAdapter: ResourceView {
                 resourceView: WeakRefVirtualProxy(view),
                 loadingView: WeakRefVirtualProxy(view),
                 errorView: WeakRefVirtualProxy(view),
-                mapper: { data in
-                    guard let image = UIImage(data: data) else {
-                        throw InvalidImageData()
-                    }
-                    return image
-                }
+                mapper: UIImage.tryMake
             )
 
             return view
@@ -46,4 +41,14 @@ final class FeedViewAdapter: ResourceView {
     }
 }
 
-private struct InvalidImageData: Error {}
+extension UIImage {
+    struct InvalidImageData: Error {}
+
+    static func tryMake(data: Data) throws -> UIImage {
+        guard let image = UIImage(data: data) else {
+            throw InvalidImageData()
+        }
+
+        return image
+    }
+}
