f794517ff4c685285a66eb370f24431fff3d055b
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-11 22:19:09 +0300

    Inline param

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -25,10 +25,10 @@ final class FeedPresenterTests: XCTestCase {
 
     private func localized(
         _ key: String,
-        table: String = "Feed",
         file: StaticString = #file,
         line: UInt = #line
     ) -> String {
+        let table = "Feed"
         let bundle = Bundle(for: FeedPresenter.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
         if value == key {
