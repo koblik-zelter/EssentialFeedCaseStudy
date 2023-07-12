79dd9671770d9e9d4f0b7423983e43acb324bb98
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-12 22:35:00 +0300

    Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Alexandr Coblic-Zelter on 12.07.2023.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet public private(set) var messageLabel: UILabel!
+    @IBOutlet public private(set)  var usernameLabel: UILabel!
+    @IBOutlet public private(set) var dateLabel: UILabel!
+}
