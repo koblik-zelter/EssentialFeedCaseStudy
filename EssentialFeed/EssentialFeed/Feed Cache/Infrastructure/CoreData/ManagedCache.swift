814f663f80a312978dfedc629ac0a08db7684b0d
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-16 20:07:01 +0300

    Remove duplication

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -21,8 +21,12 @@ class ManagedCache: NSManagedObject {
 }
 
 extension ManagedCache {
+    static func deleteCache(in context: NSManagedObjectContext) throws {
+        try find(in: context).map(context.delete).map(context.save)
+    }
+
     static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
-        try find(in: context).map(context.delete)
+        try deleteCache(in: context)
         return ManagedCache(context: context)
     }
 
