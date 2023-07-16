8f4de4ed56b4a68d9cf216a8ea256496f85d21e5
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-16 17:00:53 +0300

    Load more items on will display LoadMoreCell

--- EssentialApp/EssentialApp/Composers/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/Composers/FeedViewAdapter.swift
@@ -29,8 +29,10 @@ final class FeedViewAdapter: ResourceView {
     }
 
     func display(_ viewModel: Paginated<FeedImage>) {
-        controller?.display(viewModel.items.map { model in
-            let adapter = ImageDataPresentationAdapter(loader: { [imageLoader] in imageLoader(model.url) })
+        let feed: [CellController] = viewModel.items.map { model in
+            let adapter = ImageDataPresentationAdapter(loader: { [imageLoader] in
+                imageLoader(model.url)
+            })
             let view = FeedImageCellController(
                 viewModel: FeedImagePresenter.map(model),
                 delegate: adapter,
@@ -44,9 +46,14 @@ final class FeedViewAdapter: ResourceView {
                 errorView: WeakRefVirtualProxy(view),
                 mapper: UIImage.tryMake
             )
-
+             
             return CellController(id: model, view)
-        })
+        }
+        let loadMore = LoadMoreCellController {
+            viewModel.loadMore?({ _ in })
+        }
+        let loadMoreSection = [CellController(id: UUID(), loadMore)]
+        controller?.display(feed, loadMoreSection)
     }
 }
 
--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -124,6 +124,18 @@ extension ListViewController {
         0
     }
 
+    private var feedLoadMoreSection: Int {
+        1
+    }
+
+    func simulateLoadMoreFeedAction() {
+        guard let view = cell(row: 0, section: feedLoadMoreSection) else { return }
+
+        let delegate = tableView.delegate
+        let index = IndexPath(row: 0, section: feedLoadMoreSection)
+        delegate?.tableView?(tableView, willDisplay: view, forRowAt: index)
+    }
+
     func renderedFeedImageData(at index: Int) -> Data? {
         simulateFeedImageViewVisible(at: index)?.renderedImage
     }
