fb358564d1318cf40101ed561aeafa7fae310d92
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-17 21:35:41 +0300

    Load images synchronously

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -14,13 +14,13 @@ final class FeedImageDataStoreSpy: FeedImageDataStore {
         case retrieve(dataFor: URL)
     }
 
-    private(set) var retrievalCompletions: [(FeedImageDataStore.RetrievalResult) -> Void] = []
+    private var retrievalResult: Result<Data?, Error>?
     private var insertionResult: Result<Void, Error>?
     private(set) var receivedMessages = [Message]()
 
-    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+    func retrieve(dataForURL url: URL) throws -> Data? {
         receivedMessages.append(.retrieve(dataFor: url))
-        retrievalCompletions.append(completion)
+        return try retrievalResult?.get()
     }
 
     func insert(_ data: Data, for url: URL) throws {
@@ -28,12 +28,12 @@ final class FeedImageDataStoreSpy: FeedImageDataStore {
         try insertionResult?.get()
     }
 
-    func completeRetrieval(with error: Error, at index: Int = 0) {
-        retrievalCompletions[index](.failure(error))
+    func completeRetrieval(with error: Error) {
+        retrievalResult = .failure(error)
     }
 
-    func completeRetrieval(with data: Data?, at index: Int = 0) {
-        retrievalCompletions[index](.success(data))
+    func completeRetrieval(with data: Data?) {
+        retrievalResult = .success(data)
     }
 
     func completeInsertion(with error: Error) {
--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -50,34 +50,6 @@ final class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
         })
     }
 
-    func test_loadImageDataFromURL_doesNotDeliverResultAfterCancellingTask() {
-        let (sut, store) = makeSUT()
-        let foundData = anyData()
-
-        var received = [FeedImageDataLoader.Result]()
-        let task = sut.loadImageData(from: anyURL()) { received.append($0) }
-        task.cancel()
-
-        store.completeRetrieval(with: foundData)
-        store.completeRetrieval(with: .none)
-        store.completeRetrieval(with: anyNSError())
-
-        XCTAssertTrue(received.isEmpty, "Expected no received results after cancelling task")
-    }
-
-    func test_loadImageDataFromURL_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let store = FeedImageDataStoreSpy()
-        var sut: LocalFeedImageDataLoader? = LocalFeedImageDataLoader(store: store)
-
-        var received = [FeedImageDataLoader.Result]()
-        _ = sut?.loadImageData(from: anyURL()) { received.append($0) }
-
-        sut = nil
-        store.completeRetrieval(with: anyData())
-
-        XCTAssertTrue(received.isEmpty, "Expected no received results after instance has been deallocated")
-    }
-
     // MARK: - Helpers
 
     private func makeSUT(
@@ -114,6 +86,8 @@ final class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
     ) {
         let exp = expectation(description: "Wait for load completion")
 
+        action()
+
         _ = sut.loadImageData(from: anyURL()) { receivedResult in
             switch (receivedResult, expectedResult) {
             case let (.success(receivedData), .success(expectedData)):
@@ -130,7 +104,6 @@ final class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
             exp.fulfill()
         }
 
-        action()
         wait(for: [exp], timeout: 1.0)
     }
 }
