b9ead5ddabe6a82d7ada26342554cb7a49df0f42
Alexandr Coblic-Zelter <alexandr.coblic-zelter@crunchyroll.com>
2023-07-17 22:09:06 +0300

    Make FeedImageDataLoader sync

--- EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
@@ -11,10 +11,16 @@ import EssentialFeed
 import EssentialFeediOS
 
 extension FeedUIIntegrationTests {
-    final class LoaderSpy: FeedImageDataLoader {
-        private var feedRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
+    final class LoaderSpy {
+        private(set) var feedRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
+        private(set) var loadMoreRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
+        
         var loadFeedCallCount: Int {
             feedRequests.count
+        }
+
+        var loadMoreCallCount: Int {
+            loadMoreRequests.count
         }
 
         func loadPublisher() -> AnyPublisher<Paginated<FeedImage>, Error> {
@@ -24,10 +30,6 @@ extension FeedUIIntegrationTests {
             return publisher.eraseToAnyPublisher()
         }
 
-        func completeFeedLoadingWithError(at index: Int = 0) {
-            feedRequests[index].send(completion: .failure(anyNSError()))
-        }
-        
         func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
             feedRequests[index].send(Paginated(items: feed, loadMorePublisher: { [weak self] in
                 self?.loadMorePublisher() ?? Empty().eraseToAnyPublisher()
@@ -35,12 +37,8 @@ extension FeedUIIntegrationTests {
             feedRequests[index].send(completion: .finished)
         }
 
-        // MARK: - LoadMoreFeedLoader
-
-        private var loadMoreRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
-
-        var loadMoreCallCount: Int {
-            loadMoreRequests.count
+        func completeFeedLoadingWithError(at index: Int = 0) {
+            feedRequests[index].send(completion: .failure(anyNSError()))
         }
 
         func loadMorePublisher() -> AnyPublisher<Paginated<FeedImage>, Error> {
@@ -65,35 +63,32 @@ extension FeedUIIntegrationTests {
 
         // MARK: - FeedImageDataLoader
 
-        private struct TaskSpy: FeedImageDataLoaderTask {
-            let cancelCallback: () -> Void
-            func cancel() {
-                cancelCallback()
-            }
-        }
+        private(set) var imageRequests = [(url: URL, publisher: PassthroughSubject<Data, Error>)]()
 
-        private var imageRequests = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
         var loadedImageURLs: [URL] {
             imageRequests.map { $0.url }
         }
 
         private(set) var cancelledImageURLs = [URL]()
 
-        func loadImageData(
-            from url: URL,
-            completion: @escaping (FeedImageDataLoader.Result) -> Void
-        ) -> FeedImageDataLoaderTask {
-            imageRequests.append((url, completion))
-            return TaskSpy { [weak self] in self?.cancelledImageURLs.append(url) }
+        func loadImageDataPublisher(from url: URL) -> AnyPublisher<Data, Error> {
+            let publisher = PassthroughSubject<Data, Error>()
+            imageRequests.append((url, publisher))
+
+            return publisher
+                .handleEvents(receiveCancel: { [weak self] in
+                    self?.cancelledImageURLs.append(url)
+                })
+                .eraseToAnyPublisher()
         }
 
         func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
-            imageRequests[index].completion(.success(imageData))
+            imageRequests[index].publisher.send(imageData)
+            imageRequests[index].publisher.send(completion: .finished)
         }
 
         func completeImageLoadingWithError(at index: Int = 0) {
-            let error = NSError(domain: "an error", code: 0)
-            imageRequests[index].completion(.failure(error))
+            imageRequests[index].publisher.send(completion: .failure(anyNSError()))
         }
     }
 }
